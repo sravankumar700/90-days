bf905fe49e5cb9da3f5e3d0cba5a62b037cfed07
Sravankumar700 <chintux66@gmail.com>
2026-07-02 19:47:19 +0530

    first commit

---
+++ d1.py
@@ -0,0 +1,29 @@
+a = int(5)
+b= int(20)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+X = 2
+X += 3
+Y = 3
+Y += X
+Z = 10
+print(X,Y)
+print(X + Z ,Y +  Z)
+
+name = "sravan"
+print(name)
+name + "kumar"
+print(name[5])
+print(name[-4])
+print(name[0:3])
+# print(name[4:])    / /// there is no ending so it prints till last 
+# print(name[:2])   //there is no begining so it starts printing from start
+print(name[0:3] + "Kumar")
+
+myname ="sravan kumar"
+print(len(myname))
