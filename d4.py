6f86016e094a32b7ac869578e96a5827e6d5066f
Sravankumar700 <chintux66@gmail.com>
2026-07-05 22:13:18 +0530

    july d5.py

--- d4.py
+++ d4.py
@@ -1,22 +1,22 @@
-#/////////////                                               Dictionaries in detail                              ///////////////
-# dit = {"sravan":"java","chandu":"js","hari":"c#"}
-# print(dit)
-# print(dit.keys())
-# print(dit.values())
+# /////////////                                         //      Dictionaries in detail                              ///////////////
+dit = {"sravan":"java","chandu":"js","hari":"c#"}
+print(dit)
+print(dit.keys())
+print(dit.values())
 
 
 
 
-# # ADVANCED DICTIONARIE WITH LIST #
-# keys = ['sravan','chandu','hari']
-# valu = ['python','cloud','powerbi']
-# dic = dict(zip(keys,valu))
-# print(dic)
+# ADVANCED DICTIONARIE WITH LIST #
+keys = ['sravan','chandu','hari']
+valu = ['python','cloud','powerbi']
+dic = dict(zip(keys,valu))
+print(dic)
 
-# dic['kittu'] = "excel"
-# print(d ic)
-# del dic['sravan']
-# print(dic)
+dic['kittu'] = "excel"
+print(d ic)
+del dic['sravan']
+print(dic)
 
 
 data = {'js':'nodejs','python':['pycharm','vscode,','subline'],'java':{'JSE':'jdk','JEE':'sravan'}}
--- d5.py
+++ d5.py
@@ -1,1 +1,11 @@
-dict
+x= 2
+y= 4
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+x=x+2
+x+=2
+print(x)
+x*=3
+print(x)
