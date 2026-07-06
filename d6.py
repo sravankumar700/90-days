1063390a36812ec3f4e5251d16970b2f911d5cb7
Sravankumar700 <chintux66@gmail.com>
2026-07-06 22:39:04 +0530

    july 06 first task

--- d6.py
+++ d6.py
@@ -3,24 +3,54 @@
 # 2) DECIMAL ----
 # 3) OCTAL   ----USES OCT() FUNCTION TO CONVERT FROM ANY TO OCTAL AND REPRESENTED BY "0O" TO SAY THAT ITS A OCTAL NUMBER
 # 4) HEXA DECIMAL ----USES HEX() FUNCTION TO CONVERT FROM ANY TO HEXADECIMAL AND REPRESENTED BY "0X" TO SAY THAT ITS A HEXADECIMAL  NUMBER
-bin(30)
-print(bin(117))
-print(0b1110101)
-print(oct(295432))
-print(0o1101010)
-print(hex(295432))
-print(0x48208)
+# bin(30)
+# print(bin(117))
+# print(0b1110101)
+# print(oct(295432))
+# print(0o1101010)
+# print(hex(295432))
+# print(0x48208)
 
 
 
 
-####### important math Func()  #######
-import math
-print(math.sqrt(25))
-print(math.floor(2.9))
-print(math.ceil(2.9))
-print(math.floor(12.9))
-print(math.ceil(12.9))
-print(math.floor(222.9))
-print(math.ceil(222.9))
-print(m.pow(3,2))
+# ####### important math Func()  #######
+# import math
+# print(math.sqrt(25))
+# print(math.floor(2.9))
+# print(math.ceil(2.9))
+# print(math.floor(12.9))
+# print(math.ceil(12.9))
+# print(math.floor(222.9))
+# print(math.ceil(222.9))
+# print(m.pow(3,2))
+
+
+### Swapping 2 variables in python ########
+# a = 5 
+# b = 6
+# print(a,b)
+# a,b = b,a
+# print(a,b)
+# a =a+b
+# b = a -b
+# a = a -b
+# print(a,b)
+
+
+##### User INput in Python #########
+# if user enter any thing in the input function in the output that will be stored as string we eed to change the datatype of input function
+
+
+# x= int(input("Enter 1st Number :"))
+# y= int(input("Enter 2nd Number :"))
+# z = x + y
+# print(z)
+
+
+ch = input("Enter a Character :")[0]
+print(ch)
+result = eval(input("enter an expresssion:"))
+print(result)
+
+# Passing values from command line
