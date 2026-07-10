32020fd01a79b26999961201a9409eedd36b7ac8
Sravankumar700 <chintux66@gmail.com>
2026-07-10 22:06:55 +0530

    message 1

--- day9.py
+++ day9.py
@@ -1,15 +1,15 @@
 # ######### FOR ELSE ###############
 # # in c,c++,java there will be if else are together not for else here in python we have for else in python for a entire loop if it not works will goes for else block
 
-# i =[13,21,51,65,57]
+i =[13,21,51,65,57]
 
-# for i in i :
-#     if i % 2 == 0: # here your if block checks for number divisible by 2
-#     	print(i)
-#     else :   # here else block checks for if block if there no elemnts found at if bock then else bloak activates regulary and prints not found 5 times
-#         print(" Not Found")
-# else :  # here the else block works for entire for loop and prints only else print stmt
-#     print("There is No NUmber Divisible By YOur Requirements")
+for i in i :
+    if i % 2 == 0: # here your if block checks for number divisible by 2
+    	print(i)
+    else :   # here else block checks for if block if there no elemnts found at if bock then else bloak activates regulary and prints not found 5 times
+        print(" Not Found")
+else :  # here the else block works for entire for loop and prints only else print stmt
+    print("There is No NUmber Divisible By YOur Requirements")
 
 
 
@@ -17,16 +17,16 @@
 
 # ######## prime number logic ######
 
-# num = int(input("Enter the Number"))
-# for i in range(2,num) :
-#     if num   % i == 0:
-#         print("Not Prime")
-#         break
-# else :
-#     print(" Its a Prime Number")
+num = int(input("Enter the Number"))
+for i in range(2,num) :
+    if num   % i == 0:
+        print("Not Prime")
+        break
+else :
+    print(" Its a Prime Number")
 
 
-# num = int(input("Enter a Number"))
+num = int(input("Enter a Number"))
 num = 29
 num = 29
 for i in range(num):
@@ -37,35 +37,35 @@ else:
     print("Its a Prime Number")
 
 
-# lst = [12,18,45,67,90]
-# for i in lst :
-#     if i ==45 :
-#         print("Number Found")
-#         break
-# else:
-#     print('Not Found')
+lst = [12,18,45,67,90]
+for i in lst :
+    if i ==45 :
+        print("Number Found")
+        break
+else:
+    print('Not Found')
 
 
 
-# skills = ['java','c','python','IBM','Go']
-# for i in skills :
-#     if i == 'python' or 'IBM':
-#         print("Python" , "IBM")
-#         break
-# else:
-#     print("Not Found")
+skills = ['java','c','python','IBM','Go']
+for i in skills :
+    if i == 'python' or 'IBM':
+        print("Python" , "IBM")
+        break
+else:
+    print("Not Found")
 
 
 
-# for i in range(50,101) :
-#     if i % 1 == 0 and i % i == 0:
-#         print(i)
+for i in range(50,101) :
+    if i % 1 == 0 and i % i == 0:
+        print(i)
 
 
-# scores = [45,62,78,91,55,83,49]
-# for i in scores :
-#     if i >=90:
-#         print("Excellent Candidate Found")
-#         break
-# else :
-#     print("NO Excellent Candidate Found")
+scores = [45,62,78,91,55,83,49]
+for i in scores :
+    if i >=90:
+        print("Excellent Candidate Found")
+        break
+else :
+    print("NO Excellent Candidate Found")
