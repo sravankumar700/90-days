0f4d85a05b5f2f64a196ae9a60fef0d400b9732f
Sravankumar700 <chintux66@gmail.com>
2026-07-11 22:29:36 +0530

    message

---
+++ d9d10.py
@@ -0,0 +1,14 @@
+from array import *
+values = array('i',(10,20,30,40,50))
+for e in values :
+    print(e)
+
+
+character = array ('u',['A','B','C','D','E'])
+for e in character :
+    print(e)
+print(typecodes)
+
+
+user = array(int(input(" Enter Any % Integers")))
+print(user)
