087e2da64c935422b04070c8b5ff6264b1f948e6
Sravankumar700 <chintux66@gmail.com>
2026-07-19 21:53:39 +0530

    matric addidtion and operations

--- d14.py
+++ d14.py
@@ -1,31 +1,40 @@
 ####### Working With Matrix in Python #####
 
 from numpy import * 
-arr1 = array([
-                [1,2,3,5,4,7],
-                [4,5,4,5,9,6]
-])
+# arr1 = array([
+#                 [1,2,3,5,4,7],
+#                 [4,5,4,5,9,6]
+# ])
 
-arr2 = arr1.flatten()
-arr3 = arr2.reshape(3,2,2)
-print(arr1)
-print(arr1.dtype)
-print(arr1.ndim)
-print(arr1.shape)
-print(arr1.size)
-print(arr2)
-print(arr3)
+# arr2 = arr1.flatten()
+# arr3 = arr2.reshape(3,2,2)
+# print(arr1)
+# print(arr1.dtype)
+# print(arr1.ndim)
+# print(arr1.shape)
+# print(arr1.size)
+# print(arr2)
+# print(arr3)
 
 
 
-m = matrix(arr1)
-n = matrix('1,2;3,4;5,6;7,8')
+# m = matrix(arr1)
+# n = matrix('1,2;3,4;5,6;7,8')
     # n1 = matrix('1,2,3;4,5,6;7,8,9')
     # n2 = matrix('1,2,3;4,5,6')
-print(m)
+# print(m)
     # print(n)
     # print(n1)
     # print(n2)
-print(diagonal(m))
+# print(diagonal(m))
 # print(diagonal(n))
 # print(diagonal(n2))
+
+
+
+m1 =  matrix('1 2 3')
+m2 = matrix('4 5 9')
+m3 = m1+m2;
+m3 = m1*m2;
+m3 = m1/m2;
+print(m3) 
