a58389c13d8e22e4147fc34a41a9591c25d18c7a
Sravankumar700 <chintux66@gmail.com>
2026-07-19 21:45:14 +0530

    matric addidtion and operations

--- d14.py
+++ d14.py
@@ -7,11 +7,25 @@ arr1 = array([
 ])
 
 arr2 = arr1.flatten()
-arr3 = arr2.reshape(3,4)
+arr3 = arr2.reshape(3,2,2)
 print(arr1)
 print(arr1.dtype)
 print(arr1.ndim)
 print(arr1.shape)
 print(arr1.size)
-print(arr1.size)
 print(arr2)
+print(arr3)
+
+
+
+m = matrix(arr1)
+n = matrix('1,2;3,4;5,6;7,8')
+    # n1 = matrix('1,2,3;4,5,6;7,8,9')
+    # n2 = matrix('1,2,3;4,5,6')
+print(m)
+    # print(n)
+    # print(n1)
+    # print(n2)
+print(diagonal(m))
+# print(diagonal(n))
+# print(diagonal(n2))
