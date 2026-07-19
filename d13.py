568243bdd75aa90fca4677e49aaaf79d6f356459
Sravankumar700 <chintux66@gmail.com>
2026-07-19 21:23:50 +0530

    copying matrix

--- d13.py
+++ d13.py
@@ -1,11 +1,17 @@
 from numpy import * 
 arr1 = array([1,2,3,4,5])
+arr2 =arr1 . copy()
+arr1[1] = 7
 
+print(arr1)
+print(arr2)
 print(sin(arr1))
 print(cos(arr1))
 print(tan(arr1))
 print(log(arr1))
 print(sum(arr1))
 print(sqrt(arr1))
+print(id(arr2))
+print(id(arr2))
 
 
