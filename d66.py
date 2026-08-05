59646620f64342ac26b5525f12af7a6bfb7bd1ea
Sravankumar700 <chintux66@gmail.com>
2026-08-05 23:38:50 +0530

    Sravan

---
+++ d66.py
@@ -0,0 +1,70 @@
+from math import *
+
+def square(n):
+    return n * n
+# print(square(5))
+
+def add(a, b):
+    return a + b
+    results = add(10,20)
+    return(results)  
+def is_even(n):
+    # Write your code here
+    is_even(4)
+    return(True)
+
+def is_even(n) :
+    if n %2 == 0:
+        return(True)
+    else :
+        return(False)
+
+def is_positive(n):
+    if n < 0 :
+        return(False)
+    else   :
+            return(True)
+
+
+def count_even(numbers):
+    numbers=[2,5,4,7,8,9,6,10]
+    for i in range :
+            if numbers %2 ==0 :
+                return(True)
+            else:
+                return(False)
+
+
+
+            
+# def count_positive(numbers):
+#     count = 0
+#     for number in numbers :
+#         if number >0 :
+#             return(True)
+#         count = count+1
+#         elif  number ==0 :
+#              return(True)
+#         else :
+#             return(False)
+#         return count
+
+def sum_positive(numbers):
+    total= 0 
+    for number in numbers :
+        if number >0 :
+            total = number + total
+            return(count)
+
+
+
+
+
+
+
+
+
+
+
+
+D67.PY
